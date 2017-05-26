ca1c2946cd2540d8ffb6f55912e6cd9922661d38
Kitai <jeremyzehr@wal-apn.317.wlan.wireless-pennnet.upenn.edu>
2017-05-26 14:42:04 -0400

    Creating

---
+++ data_includes/example_data.js
@@ -0,0 +1,103 @@
+// Sequence of the experiment - refers to 'items' defined below
+var shuffleSequence = seq("Instructions",
+                          rshuffle("Practice"),
+                          "Start",
+                          "Experimental1",
+                          "Break",
+                          "Experimental2",
+                          "Final");
+
+// Defaults for types of questions found in this experiment
+// NOTE: 'LQuestion' (written by Akiva and/or Jeremy) is a timed version of Question
+var defaults = [
+    "LQuestion", {
+             as: ["F","J"],
+             randomOrder: false,
+             showNumbers: false,
+             presentHorizontally: true,
+             autoFirstChar: true,
+             leftComment: "<b>F</b>: Word", 
+             rightComment: "<b>J</b>: Not a word"},
+    "Message", {hideProgressBar: true},
+    "Separator", {
+            transfer: Math.random() * (600 - 400) + 400, // need to record ISI
+            normalMessage: "",
+            errorMessage: ""}
+];
+
+// 'LexDecision' is a new question type extending 'LQuestion', used for web display
+define_ibex_controller({
+    name: "LexDecision",
+    properties: {},
+    jqueryWidget: {
+        _init: function () {
+            this.options.transfer = null;
+            this.element.VBox({
+                options: this.options,
+                triggers: [1],
+                children: [
+                    "Message", {html: '<html><div style="text-align: center; margin:auto;">  <audio controls autoplay preload="auto" style="display: none;">    <source src="http://www.ling.upenn.edu/~amygood/files/soundfiles/'+ this.options.word + '" type="audio/wav" />    We are sorry but your system does not support the audio.</audio></div></html>', transfer: null },
+                    //"LQuestion", {q: this.options.word}] ORIGINAL
+                    "LQuestion", {q: this.options.word, hasCorrect: parseInt(this.options.correctResponse)}] //results are coded backwards! 0 should =word, 1 should =nw
+                });
+            }
+        }
+}); 
+
+// Using 'ScriptToPopulateFromCSV.js' written by Jeremy, load in the practice data file
+// Trying to include group number and accuracy in results output
+var practiceItems = 
+  GetItemFrom(practice, null, {
+    ItemGroup: "item",
+    Where: function(row){return row.group==1;},
+    Elements: [ 
+        function(row){return "Practice";},              // Name of the item, when group number included half the stims only??
+        function(row){return "LexDecision";},           // First Controller: LexDecision
+        {word: "psf", correctResponse: "primeNWStatus"}, // correctResponse was hasCorrect
+        function(row){return "Separator";},             // Second Controller: Separator 
+        {},
+        function(row){return "LexDecision";},           // Third Controller: LexDecision
+        {word: "tsf", correctResponse: "targetNWStatus"},
+        function(row){return "Separator";},             // Fourth Controller: Separator
+        {}]
+});
+
+// Declare a random group number for the participant
+var groupNum = Math.floor(1+6*Math.random()); // randomly generate subject number
+//var groupNum = 6; // set as group 2
+
+// Using 'ScriptToPopulateFromCSV.js' written by Jeremy, load in the experimental data file
+var exptItems = 
+  GetItemFrom(data, null, {
+    ItemGroup: "item",
+    Where: function(row){return row.group==groupNum;},
+    Elements: [ 
+        function(row){return "Experimental"+row.block;},// Name of the item
+        function(row){return "LexDecision";},           // First Controller: LexDecision
+        {word: "psf", correctResponse: "primeNWStatus"},
+        function(row){return "Separator";},             // Second Controller: Separator 
+        {},
+        function(row){return "LexDecision";},           // Third Controller: LexDecision
+        {word: "tsf", correctResponse: "targetNWStatus"},
+        function(row){return "Separator";},             // Fourth Controller: Separator
+        {}]
+}); 
+
+// Declare the format of the experiment
+var items = [
+    ["Instructions", 
+     "Message", {transfer: "click", html: {include: "consentform.html"}},
+     "Message", {transfer: "click", html: {include: "warning.html"}},
+     "Message", {transfer: "click", html: {include: "instructions.html"}},
+     "Question", {q: groupNum, as: ['Group num is','GroupNum'], timeout:1}],
+    ["Start","Message",{html: {include: "start.html"}}],
+    ["Break","Message",{html: "<html><div><p>If you want, you can now take a break.</p></div></html>"}],
+    ["Final", 
+     "Form", {continueMessage: "Click here to continue...", html: {include: "amt_form.html"}},
+     "__SendResults__", {manualSendResults: true, sendingResultsMessage: "Please wait while your answers are being saved...", completionMessage: "Your answers have successfully been saved! You may now close this window."},
+     "Message", {transfer: null, html: {include: "final.html"}}]
+]
+
+// Add in the practice and the experimental items
+items = items.concat(practiceItems)
+items = items.concat(exptItems)
---
+++ js_includes/bt9_Practice.js
@@ -0,0 +1,13 @@
+practice = [
+  ["group","item","block","psf","tsf","prime","target","ptype","ttype","primeNWStatus","targetNWStatus"  ],
+  ["1","1","1","nw_skrown_R.wav","w_gnome_R.wav","skrown","gnome","NW-Practice","W-Practice","1","0"  ],
+  ["1","2","1","nw_grihjh_R.wav","w_tripe_R.wav","grihjh","tripe","NW-Practice","W-Practice","1","0"  ],
+  ["1","3","1","w_plum_R.wav","nw_gaech_R.wav","plum","gaech","W-Practice","NW-Practice","0","1"  ],
+  ["1","4","1","w_slab_R.wav","nw_kwaanch_R.wav","slab","kwaanch","W-Practice","NW-Practice","0","1"  ],
+  ["1","5","1","nw_klihjh_R.wav","nw_sliyn_R.wav","klihjh","sliyn","NW-Practice","NW-Practice","1","1"  ],
+  ["1","6","1","nw_frayn_R.wav","nw_fiyk_R.wav","frayn","fiyk","NW-Practice","NW-Practice","1","1"  ],
+  ["1","7","1","nw_swiyn_R.wav","nw_niyk_R.wav","swiyn","niyk","NW-Practice","NW-Practice","1","1"  ],
+  ["1","8","1","w_clay_R.wav","w_flame_R.wav","clay","flame","W-Practice","W-Practice","0","0"  ],
+  ["1","9","1","w_beer_R.wav","w_fluff_R.wav","beer","fluff","W-Practice","W-Practice","0","0"  ],
+  ["1","10","1","w_bridge_R.wav","w_lump_R.wav","bridge","lump","W-Practice","W-Practice","0","0"  ]
+]
